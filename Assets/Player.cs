34771d6606c1960f425ec00b49c99161ad417f7c
SlickJohnson <liwa.johnson@gmail.com>
2019-03-20 00:23:43 -0700

    Add clipped ship movement

---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+  [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+  [Tooltip("In m")] [SerializeField] float xRange = 5f;
+
+  [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4f;
+  [Tooltip("In m")] [SerializeField] float yRange = 5f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+    float xOffset = xThrow * xSpeed * Time.deltaTime;
+    float rawNewXPos = transform.localPosition.x + xOffset;
+    float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+    float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+    float yOffset = yThrow * ySpeed * Time.deltaTime;
+    float rawNewYPos = transform.localPosition.y + yOffset;
+    float clampedYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+    transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+  }
+}
