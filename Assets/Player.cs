1c7ac7b78caf84ca536caa2a064d57c91a6278b7
SlickJohnson <liwa.johnson@gmail.com>
2019-03-20 01:00:53 -0700

    Tweak control values

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,6 +9,13 @@ public class Player : MonoBehaviour
   [Tooltip("In m")] [SerializeField] float xRange = 5f;
   [Tooltip("In m")] [SerializeField] float yRange = 3f;
 
+  [SerializeField] float positionPitchFactor = -5f;
+  [SerializeField] float controlPitchFactor = -20f;
+  [SerializeField] float positionYawFactor = 5f;
+  [SerializeField] float controlRollFactor = -20f;
+
+  float xThrow, yThrow;
+
   // Start is called before the first frame update
   void Start()
   {
@@ -18,16 +25,34 @@ public class Player : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+    ProcessTranslation();
+    ProcessRotation();
+  }
+
+  private void ProcessTranslation()
+  {
+    xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
     float xOffset = xThrow * speed * Time.deltaTime;
     float rawXPos = transform.localPosition.x + xOffset;
     float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
-    float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+    yThrow = CrossPlatformInputManager.GetAxis("Vertical");
     float yOffset = yThrow * speed * Time.deltaTime;
     float rawYPos = transform.localPosition.y + yOffset;
     float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
     transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+  }
+
+  private void ProcessRotation()
+  {
+    float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+    float pitchDueToControlThrow = yThrow * controlPitchFactor;
+    float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+    float yaw = transform.localPosition.x * positionYawFactor;
+    float roll = xThrow * controlRollFactor;
+
+    transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
   }
 }
